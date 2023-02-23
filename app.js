afae065de81dedb1b10ca5a840409cca38aac104
deadclown09 <mesandesh.2014@gmail.com>
2023-02-23 14:56:50 +0545

    Changed del icon and modified UI a little

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 //1st method
 
-let screen = document.querySelector('.display');
-let buttons = document.querySelectorAll('button');
+let screen = document.querySelector(".display");
+let buttons = document.querySelectorAll("button");
 
-buttons.forEach((button)=>{
-    button.addEventListener('click', (e)=>{
-        if (e.target.innerHTML === 'C') {
-            screen.value = "";
-        }
-        else if (e.target.innerHTML === '=') {
-            if (screen.value !== "") {
-                screen.value = eval(screen.value);
-                //use try and catch to show syntax error message
-                console.log(screen.value);
-            }
-        }
-        else if (e.target.innerHTML === 'Del') {
-            let temp = screen.value.slice(0, -1);
-            screen.value = temp;
-        }
-        else if (e.target.innerHTML === '%') {
-            let temp = eval(screen.value);
-            screen.value = temp/100;
-        }
-        else {
-            screen.value += e.target.dataset.num;
-        }
-    });
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "C") {
+      screen.value = "";
+    } else if (e.target.innerHTML === "=") {
+      if (screen.value !== "") {
+        screen.value = eval(screen.value);
+        //use try and catch to show syntax error message
+        console.log(screen.value);
+      }
+    } else if (e.target.matches(".delete")) {
+      let temp = screen.value.slice(0, -1);
+      screen.value = temp;
+    } else if (e.target.innerHTML === "%") {
+      let temp = eval(screen.value);
+      screen.value = temp / 100;
+    } else {
+      screen.value += e.target.dataset.num;
+    }
+  });
 });
